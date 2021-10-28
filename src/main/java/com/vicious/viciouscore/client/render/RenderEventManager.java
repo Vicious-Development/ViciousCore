7e164736ecbe06919b9d0b274940e491a6d6ec2a
Drathonix <jacksfunny10@gmail.com>
2021-10-28 11:14:15 -0400

    MobSpawnModifier now supports multiple mods applying changes.

--- src/main/java/com/vicious/viciouscore/client/render/RenderEventManager.java
+++ src/main/java/com/vicious/viciouscore/client/render/RenderEventManager.java
@@ -1,8 +1,12 @@
 package com.vicious.viciouscore.client.render;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketUpdateBossInfo;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class RenderEventManager {
@@ -19,4 +23,16 @@ public class RenderEventManager {
         }
         angle+=0.05;
     }
+    @SubscribeEvent
+    public static void onRenderOverlay(RenderGameOverlayEvent ev){
+        Item held = Minecraft.getMinecraft().player.getHeldItemMainhand().getItem();
+        if(held instanceof IRenderOverride){
+            ((IRenderOverride) held).cancelRenderOverlays(ev);
+        }
+    }
+    @SubscribeEvent
+    public static void onRenderBossOverlay(RenderGameOverlayEvent.BossInfo ev){
+        new SPacketUpdateBossInfo().getUniqueId()
+    }
+
 }
--- src/main/java/com/vicious/viciouscore/common/modification/MobSpawnModifier.java
+++ src/main/java/com/vicious/viciouscore/common/modification/MobSpawnModifier.java
@@ -1,5 +1,6 @@
 package com.vicious.viciouscore.common.modification;
 
+import com.google.common.collect.Lists;
 import com.vicious.viciouscore.common.registries.VItemRegistry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.*;
@@ -9,6 +10,7 @@ import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -16,28 +18,37 @@ import java.util.function.Consumer;
  * Used to modify mobs on spawn. To do so you can add to the entityModificator map a new handler.
  */
 public class MobSpawnModifier {
-    public static Map<Class<? extends EntityMob>, Consumer<EntityMob>> entityModificators = new HashMap<>();
+    public static Map<Class<? extends EntityMob>, List<Consumer<EntityMob>>> entityModificators = new HashMap<>();
     public static float weaponReplaceChance = 0F;
+    //Set this to true to prevent other mods from modifying the entity.
+    public static boolean cancelOtherOverrides = false;
     //Override this to customize
-    public static Consumer<EntityMob> bipedOverrider = (EntityMob e)->{
+    public static List<Consumer<EntityMob>> bipedOverriderList = Lists.newArrayList((EntityMob e)->{
         double rand = Math.random();
         if(rand > weaponReplaceChance) {
             e.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(VItemRegistry.ENERGO_RIFLE, 1));
+            MobSpawnModifier.cancelOtherOverrides = true;
         }
-    };
+    });
     static{
-        entityModificators.put(EntityZombie.class,bipedOverrider);
-        entityModificators.put(EntityZombieVillager.class,bipedOverrider);
-        entityModificators.put(EntitySkeleton.class,bipedOverrider);
-        entityModificators.put(EntityPigZombie.class,bipedOverrider);
+        entityModificators.put(EntityZombie.class,bipedOverriderList);
+        entityModificators.put(EntityZombieVillager.class,bipedOverriderList);
+        entityModificators.put(EntitySkeleton.class,bipedOverriderList);
+        entityModificators.put(EntityPigZombie.class,bipedOverriderList);
     }
     @SubscribeEvent
     public static void onMobSpawn(EntityEvent.EntityConstructing ev){
         Entity ent = ev.getEntity();
         //lol funny name
-        Consumer<EntityMob> mobEater = entityModificators.get(ent.getClass());
+        List<Consumer<EntityMob>> mobEater = entityModificators.get(ent.getClass());
         if(mobEater != null){
-            mobEater.accept((EntityMob) ent);
+            for (Consumer<EntityMob> entityMobConsumer : mobEater) {
+                if(cancelOtherOverrides){
+                    cancelOtherOverrides = false;
+                    return;
+                }
+                entityMobConsumer.accept((EntityMob) ent);
+            }
         }
     }
 }
