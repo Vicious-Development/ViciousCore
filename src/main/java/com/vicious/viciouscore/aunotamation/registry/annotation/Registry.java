92d87957ff01e35944b1076cba4cd95208318200
Drathonix <52723768+Drathonix@users.noreply.github.com>
2022-10-19 10:12:13 -0400

    3.8.0 - Further reduced @LinkBE code requirements.

--- src/main/java/com/vicious/viciouscore/aunotamation/registry/annotation/Registry.java
+++ src/main/java/com/vicious/viciouscore/aunotamation/registry/annotation/Registry.java
@@ -2,7 +2,7 @@ package com.vicious.viciouscore.aunotamation.registry.annotation;
 
 import com.vicious.viciouslib.aunotamation.annotation.ModifiedWith;
 import com.vicious.viciouslib.aunotamation.annotation.RequiredType;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.IForgeRegistry;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,12 +11,13 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Modifier;
 
 /**
- * Marks the DeferredRegister in a registry class.
+ * Marks the IForgeRegistry in a registry class.
  * There can only be one!
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @ModifiedWith({Modifier.STATIC, Modifier.PUBLIC})
-@RequiredType(RegistryObject.class)
+@RequiredType(IForgeRegistry.class)
 public @interface Registry {
+    String value();
 }
