53abf9535baeb35892adf5b0197ba69d89f20ced
Drathonix <52723768+Drathonix@users.noreply.github.com>
2022-10-19 21:37:48 -0400

    V 3.8.8 - Universalized @LinkBE for all registries, replacing it with @Register(TARGETCLASS).

--- src/main/java/com/vicious/viciouscore/aunotamation/registry/RegistryAutomator.java
+++ src/main/java/com/vicious/viciouscore/aunotamation/registry/RegistryAutomator.java
@@ -1,105 +1,42 @@
 package com.vicious.viciouscore.aunotamation.registry;
 
 import com.vicious.viciouscore.ViciousCore;
-import com.vicious.viciouscore.aunotamation.registry.annotation.BlockAssociations;
-import com.vicious.viciouscore.aunotamation.registry.annotation.LinkBE;
-import com.vicious.viciouscore.aunotamation.registry.annotation.Registry;
-import com.vicious.viciouslib.aunotamation.AnnotationProcessor;
 import com.vicious.viciouslib.aunotamation.Aunotamation;
-import com.vicious.viciouslib.util.ClassAnalyzer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.RegistryObject;
 
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Locale;
 
 public class RegistryAutomator {
+    public static final RegistryAnnotationProcessor UNIVERSALREGISTRYPROCESSOR = new RegistryAnnotationProcessor();
     private static boolean init = false;
     public static void init() {
         if (init) return;
         init = true;
-        //ALLOWS THIS:
-        /*
-        class ExampleTileRegistry{
-            @Registry
-            public static final DeferredRegister<BlockEntityType<?>> BLOCKENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, "example");
-            @LinkBE(ExampleBE.class)
-            public static RegistryObject<BlockEntityType<ExampleBE>> EXAMPLEBLOCKENTITY;
-        }
-
-        @BlockAssociations("exampleblock",namespace = "examplemod")
-        class ExampleBE extends BlockEntity{...}
-         */
-        Aunotamation.registerProcessor(new AnnotationProcessor<>(LinkBE.class, Class.class) {
+        UNIVERSALREGISTRYPROCESSOR.addSupport(new RegistryProcessor<>(BlockEntity.class, ForgeRegistries.BLOCK_ENTITY_TYPES,ForgeRegistries.BLOCKS, BlockPos.class, BlockState.class) {
             @Override
-            @SuppressWarnings("unchecked")
-            public void process(Class registryClass, AnnotatedElement annotatedElement) throws Exception {
-                if(annotatedElement instanceof Field f) {
-                    //Get the DeferredRegister.
-                    List<AnnotatedElement> possibilities = ClassAnalyzer.getManifest(registryClass).getMembersWithAnnotation(Registry.class);
-                    if(possibilities.size() == 0){
-                        err(annotatedElement,"there is no @Registry DeferredRegister");
-                    }
-                    if(possibilities.size() > 1){
-                        err(annotatedElement,"there is more than one @Registry DeferredRegister");
-                    }
-                    IForgeRegistry<BlockEntityType<?>> registry = (IForgeRegistry<BlockEntityType<?>>) ((Field)possibilities.get(0)).get(registryClass);
-                    DeferredRegister<BlockEntityType<?>> deferredRegister = DeferredRegister.create(registry,possibilities.get(0).getAnnotation(Registry.class).value());
-                    //Get the tile class.
-                    Class<?> tile = annotatedElement.getAnnotation(LinkBE.class).value();
+            public BlockEntityType<?> supply(Class<BlockEntity> targetClass, Constructor<BlockEntity> constructor, List<Block> associations, Field targetField) throws Exception {
+                //Create the block entity type.
+                return BlockEntityType.Builder.of((pos, state) -> {
                     try {
-                        //Get the BlockEntity Constructor.
-                        Constructor<?> constructor = tile.getConstructor(BlockPos.class, BlockState.class);
-                        RegistryObject<?> obj = deferredRegister.register(f.getName().toLowerCase(Locale.ROOT),()->{
-                            try {
-                                //Get the tile's associated blocks.
-                                BlockAssociations associations = tile.getAnnotation(BlockAssociations.class);
-                                if(associations == null) {
-                                    err(annotatedElement, " tile class is missing @BlockAssociations annotation");
-                                }
-                                String namespace = associations.namespace();
-                                String[] keys = associations.value();
-                                Block[] blocks = new Block[keys.length];
-                                for (int i = 0; i < keys.length; i++) {
-                                    blocks[i] = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(namespace, keys[i]));
-                                }
-                                //Create the block entity type.
-                                return BlockEntityType.Builder.of((p, s) -> {
-                                    Object be = null;
-                                    try {
-                                        be = constructor.newInstance(p, s);
-                                    } catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {
-                                        err(annotatedElement, "encountered internal exception while creating new tile", e);
-                                    }
-                                    Aunotamation.processObject(be);
-                                    return (BlockEntity) be;
-                                }, blocks).build(null);
-                            }
-                            catch (Exception e){
-                                ViciousCore.logger.fatal("Could register a block entity.");
-                                e.printStackTrace();
-                            }
-                            return null;
-                        });
-                        f.set(registryClass,obj);
+                        BlockEntity be = constructor.newInstance(pos, state);
+                        Aunotamation.processObject(be);
+                        return be;
+                    } catch (InvocationTargetException | InstantiationException | IllegalAccessException e){
+                        ViciousCore.logger.fatal("Failed to create a block entity. Caused by internal exception: " + e.getCause());
+                        e.printStackTrace();
                     }
-                    catch (NoSuchMethodException e){
-                        err(f,"encountered internal exception while registering tile",e);
-                    }
-                }
+                    return null;
+                }, associations.toArray(new Block[0])).build(null);
             }
         });
+        Aunotamation.registerProcessor(UNIVERSALREGISTRYPROCESSOR);
     }
 }
